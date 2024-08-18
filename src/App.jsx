3eaf04eb40ae3028b8b1ed2e4a552b60ba3e32c3
wayline89 <laurentdugardin@gmail.com>
2024-08-18 21:03:24 +0200

    premier tail installé

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+<h1 className='text-xl font-bold text-blue-500'>hello jglvn,b,nbn,b,nb </h1>
+    </>
+  )
+}
+
+export default App
