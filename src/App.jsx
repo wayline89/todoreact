9bda1e99dd70d8baf543e2f1699b43c7f2eacfcf
wayline89 <laurentdugardin@gmail.com>
2024-09-01 15:46:53 +0200

    cats

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import { useState } from "react";
-import ReactDOM from "react-dom";
-import Text from "./Text";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 
 function App() {
-  const [ShowText, setShowText] = useState(false);
 
-return(
-  <div className="App">
-<button 
-onClick={() => {
-  setShowText(!ShowText);
-}}>
-  Show Text 
-</button>
-{ShowText && <Text/>}
-  </div>
-)
+const [catFact,setCatFact] = useState("");
 
+const fetchCatFact = () => {
+  Axios.get("https://catfact.ninja/fact").then((res)=>{ 
+    setCatFact(res.data.fact)
+  });
+}
+useEffect(()=>{
+  fetchCatFact
+},[]);
+
+
+  return (
+    <div className="App">
+      <button onClick={fetchCatFact}>Generate Cat Fact</button>
+      <p>{catFact} </p>
+    </div>
+  );
 }
 
 export default App;
+
 
--- src/Text.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Text = () => {
-  return (
-<h1>Text</h1>
-  )
-}
-
-export default Text
